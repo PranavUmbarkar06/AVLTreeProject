22a0dcb66fb453679f3f1df15e193f40623e6de4
PranavUmbarkar06 <pu24csb1a50@student.nitw.ac.in>
2025-02-19 10:32:56 +0530

    Update NodeOperations.cpp

--- NodeOperations.cpp
+++ NodeOperations.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 using namespace std;
-int main(){
-  cout<<"Hello"<<endl;
-}
+struct courses{
+    int courseCode;
+    int marks;
+};
+class StudentStructure{
+public:
+    int rollNo;
+    float cg;
+    int numsubjects;
+    courses subjects[numsubjects];
+    StudentStructure* left;
+    StudentStructure* right;
+    int height;
+    StudentStructure(int rollNo,float cg,int numsubjects,courses subjects[]){
+        this->rollNo=rollNo;
+        this->cg=cg;
+        this->numsubjects=numsubjects;
+        for(int i=0;i<numsubjects;i++){
+            this->subjects[i].courseCode=subjects[i].courseCode;
+            this->subjects[i].marks=subjects[i].courseCode;
+        }
+    }
+};
