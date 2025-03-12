1d03a6b1fcb221040610d3793591ab158147fd3b
RSS11017 <mr24csb1a37@student.nitw.ac.in>
2025-03-12 08:38:54 +0530

    Update NodeOperations.cpp

--- NodeOperations.cpp
+++ NodeOperations.cpp
@@ -1,25 +1,17 @@
 #include<iostream>
 #include <string>
+#ifndef NODEOPERATION_H
+#define NODEOPERATION_H
 using namespace std;
+
 struct courses{
     int courseCode;
     int marks;
-};
-class StudentStructure{
-public:
-    int rollNo;
-    string name;
-    float cg;
-    int numsubjects;
-    courses *subjects;
-    StudentStructure* left;
-    StudentStructure* right;
-    int height;
-    StudentStructure(int rollNo,string name,float cg,int numsubjects,courses *subjects){
-        this->rollNo=rollNo;
-        this->name=name;
-        this->cg=cg;
-        this->numsubjects=numsubjects;
-        this->subjects=subjects;
+    courses(int code, float marks){
+        courseCode=code;
+        this->marks=marks;
     }
+    courses() : courseCode(0), marks(0.0f) {}
 };
+
+#endif
